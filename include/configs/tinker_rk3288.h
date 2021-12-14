1cbbeba154f6cd0b336562a889409a9df904c991
SummerSunGenius <SummerSun666888@163.com>
2021-12-14 02:38:53 -0800

    RG552

---
+++ include/configs/tinker_rk3288.h
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2016 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#include <configs/rk3288_common.h>
+
+#define CONFIG_SYS_MMC_ENV_DEV 1
+
+#endif
